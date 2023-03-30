c6425dd6ca7d900b3be86f5480c3b6e5d7bdc87e
ZephirSD <siegfrieddallery@gmail.com>
2023-03-30 15:11:59 +0400

    Création de la page CookPage

---
+++ src/pages/CookPage.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Card from '../components/Card';
+
+const CookPage = () => {
+    const [searchRepas, setSearchRepas] = useState("");
+    const [repasList, setrepasList] = useState([]);
+    const fetchRepas = () => {
+        axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchRepas)
+            .then(response => setrepasList(response.data.meals))
+    }
+    useEffect(fetchRepas, [searchRepas]);
+    return (
+        <>
+            <header>
+                <h1 className="titre">Reack Cook</h1>
+                <input type="text" onChange={(e) => setSearchRepas(e.target.value)} className='inputSearch'/>
+            </header>
+            <section className="grid_card">
+                {repasList
+                    .filter((search) => search.strMeal.toLowerCase().includes(searchRepas.toLowerCase()))
+                    .map((repas, index) => (
+                    <Card key={index} source={repas} />
+                ))}
+            </section>
+        </>
+    );
+};
+
+export default CookPage;
