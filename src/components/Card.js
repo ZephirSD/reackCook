6fa49ad9cda0fd8192b344a63f02a13adcd5368a
ZephirSD <siegfrieddallery@gmail.com>
2023-03-30 20:42:32 +0400

    Modification de style pour la carte des repas

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,16 @@ const Card = ({source}) => {
     return (
         <>
             <div className="card">
-                <div className="titre_card">{ source.strMeal}</div>
-                <div className="origin_repas">
-                    <span>Origin</span>
-                    <span>{ source.strArea }</span>
+                <div className="titre_card">{ source.strMeal }</div>
+                <div className="origin_repas" style={{marginBottom: "5px"}}>
+                    <span>Origin:</span>
+                    <span style={{marginLeft: "5px"}}>{ source.strArea }</span>
                 </div>
                 <div className="img_repas">
-                    <img src={source.strMealThumb} alt="img_plat" />
+                    <img src={source.strMealThumb } alt="img_plat" />
                 </div>
                 <div className="desc_repas">
-                    { source.strInstructions}
+                    { source.strInstructions }
                 </div>
             </div>
         </>
